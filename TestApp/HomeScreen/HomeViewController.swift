5aa978ed1d7393b91e86f88481f1a8971aba3428
mohamedkeshawy <mokeshawy@gmail.com>
2024-02-27 16:48:36 +0200

    Initial commit

--- TestApp/HomeScreen/HomeViewController.swift
+++ TestApp/HomeScreen/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  TestApp
 //
 //  Created by Mohamed Keshawy on 26/02/2024.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
---
+++ TestApp/LoginScreen/ViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class ViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTextFiledInputDelegate()
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+
+    private func setTextFiledInputDelegate(){
+        txtUsername.delegate = self
+        txtPassword.delegate = self
+    }
+    
+    
+    @IBAction func setOnLoginBtnClicked(_ sender: Any) {
+        print("\(txtUsername.text ?? "")")
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == txtUsername{
+            txtPassword.becomeFirstResponder()
+        }else{
+            // Implement login logic here
+        }
+        return true
+    }
+}
+
