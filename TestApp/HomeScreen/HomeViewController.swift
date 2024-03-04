3d2a3afadd0501aa07ab00aa0dd1d5395445fcc6
mohamedkeshawy <mokeshawy@gmail.com>
2024-03-04 10:32:41 +0200

    Implement Constraints and StackView

--- TestApp/HomeScreen/HomeViewController.swift
+++ TestApp/HomeScreen/HomeViewController.swift
@@ -2,7 +2,7 @@
 //  HomeViewController.swift
 //  TestApp
 //
-//  Created by Mohamed Keshawy on 26/02/2024.
+//  Created by Mohamed Keshawy on 03/03/2024.
 //
 
 import UIKit
@@ -12,5 +12,18 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
---
+++ TestApp/OnBoarScreen/OnBoardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  TestApp
+//
+//  Created by Mohamed Keshawy on 26/02/2024.
+//
+
+import UIKit
+
+class OnBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
